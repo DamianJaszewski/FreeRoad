b311129792e3a80b28a8c009d4cdb08097bd6dbd
DamianJaszewski <DamianJaszewski@users.noreply.github.com>
2021-04-28 18:03:05 +0200

    add main screen and route button

--- app/src/main/java/com/example/freeroad/MainActivity.java
+++ app/src/main/java/com/example/freeroad/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.freeroad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mapPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapPage = findViewById(R.id.mapButton);
+        mapPage.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openMapsActivity();
+            }
+        });
+    }
+
+    public void openMapsActivity(){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
